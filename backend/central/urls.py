63bc20cda9d0e304c1a4ec66b25034bb73345523
jinx <junrymingo.bposeats@gmail.com>
2025-09-02 16:23:21 +0800

    chore: add task to admin

--- backend/central/urls.py
+++ backend/central/urls.py
@@ -21,7 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("custom_auth.urls")),
-    path("api/", include([
-        path("v1/", include("task.urls"))
-    ]))
+    path("api/", include([path("v1/", include("task.urls"))])),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
